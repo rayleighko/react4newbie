f64cc0431351d379c2d8eb9c4641f35b4aaf80c1
rayleighko <gmj1197@gmail.com>
2020-02-10 17:15:19 +0900

    refector: write for good than before

--- todoListWithFC/README.md
+++ todoListWithFC/README.md
@@ -1,3 +1,22 @@
 # Todo List with Functional Component
 
 함수형 컴포넌트 기반 투두 리스트 어플리케이션을 만들어 봅시다.
+
+## 사용할 라이브러리(디테일)
+
+### Material UI
+
+- @material-ui/core/Typography
+- @material-ui/core/TextField
+- @material-ui/core/List
+- @material-ui/core/ListItem
+- @material-ui/core/Checkbox
+- @material-ui/icons/Delete
+- @material-ui/core/IconButton
+- @material-ui/core/ListItemText
+- @material-ui/core/ListItemSecondaryAction
+
+### React Hooks
+
+- useState
+- useEffect
--- todoListWithFC/src/componennts/App.jsx
+++ todoListWithFC/src/componennts/App.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import Typography from "@material-ui/core/Typography";
 
 import { useTodoState } from "../hooks";
@@ -10,20 +11,22 @@ import "./styles.css";
 
 export default () => {
   const { todos, addTodo, deleteTodo } = useTodoState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => setIsLoading(false), 1000);
+  }, []);
 
   return (
     <div className="container">
       <Typography component="h1" variant="h2">
-        Todos
+        {isLoading ? "Now Loading..." : "Todos"}
       </Typography>
 
       <TodoInputForm
         saveTodo={todoText => {
           const trimmedText = todoText.trim();
-
-          if (trimmedText.length > 0) {
-            addTodo(trimmedText);
-          }
+          if (trimmedText.length > 0) addTodo(trimmedText);
         }}
       />
 
